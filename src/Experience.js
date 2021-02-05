a384d0fc68b520cf76b9e73016c32d0cee29d57e
Paola Vasquez Bramlett <76532437+paolabramlett@users.noreply.github.com>
2021-02-05 08:40:50 -0600

    Added cards to the Experience component

--- src/Experience.js
+++ src/Experience.js
@@ -1,22 +1,52 @@
 import React from "react";
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Row';
-import Image from 'react-bootstrap/Image';
 
-export default function Websites() {
+export default function Experience() {
     return (
         <div>
             <h1>
-                Websites and Apps I've developed.
+                Projects
             </h1>
-            <Container>
-            <Row>
-                <Col sm>Web Design</Col>
-                <Col sm>Photography</Col>
-                <Col sm>React</Col>
-            </Row>
-            </Container>
+            <div className="row row-cols-1 row-cols-md-2 g-4">
+    <div className="col">
+    <div className="card">
+      <img src="/images/coding.jpg" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Front End Development</h5>
+        <p className="card-text">Recently graduated as a Front End and React Developer. I've been doing some freelance work. I recently developed a weather app and I made a digital menu for a restaurant</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="/images/IMG_4357.JPG" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Photography</h5>
+        <p className="card-text">As I graduated from Filmschool, I found a passion towards photgraphy. I enjoy taking photos of landscapes, buildings and portraits</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="/images/social-media.png" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Social Media Marketing</h5>
+        <p className="card-text">For the las 4 years, I've been doing some freelance social media marketing work with over 10 companies in the state of Oaxaca, Mexico.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="/images/graphicdesign.JPG" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Graphic Design</h5>
+        <p className="card-text">Also, I do several graphic design work for those companies to help the create a more professional advertising</p>
+      </div>
+    </div>
+  </div>
+</div>
         </div>
-    )
+       
+        
+
+    );
 }
