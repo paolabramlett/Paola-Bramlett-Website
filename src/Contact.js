cab1e38438a32bb86e3b179c6b29410171e808b9
Paola Vasquez Bramlett <76532437+paolabramlett@users.noreply.github.com>
2021-02-08 13:59:49 -0600

    Contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,14 +1,69 @@
 import React from "react";
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import "./Contact.css";
 import ContactForm from "./ContactForm";
 
 export default function Contact() {
     return (
         <div>
-            
+            <Container>
+            <br />
+            <br />
+                <h1>
+                    <strong>Looking to collaborate?</strong>
+                </h1>
+                <h4>
+                    Let's do it! Contact me!
+                </h4>
+                <br />
+                <br />
+                <Row xs={1} md={2}>
+                
+                    <Col>
+                        <h2>
+                            Contact Info
+                        </h2>
+                        <ul>
+                            <li>
+                                <strong>e-mail:</strong> paolabramlett@gmail.com
+                            </li>
+                            <li>
+                                <strong>Phone number:</strong> +52 (951) 408 2852
+                            </li>
+                            <li>
+                            <strong><a href="https://www.instagram.com/paolabramlett/" target="_blank" rel="noreferrer">Instagram</a></strong>
+                            </li>
+                            <li>
+                                <strong><a href="https://www.linkedin.com/in/paolabramlett/" target="_blank" rel="noreferrer">LinkedIn</a></strong>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col>
                     <ContactForm />
-                   
+                    </Col>
+                    
+                    <Col>
+                    <Form className="p-3">
+                        <Form.Group controlId="exampleForm.ControlInput1">
+                            <Form.Label className="text-white">Email address</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label className="text-white">Message</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" className="mt-3">
+                            Send
+                        </Button>
+                    </Form>
+                    </Col>
+                    
+                </Row>
+            </Container>
         </div>
     )
 }
--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,60 +1,39 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import "./ContactForm.css";
 
 export default function ContactForm (){
     return (
         <div>
-        
-            <Container>
-            <Row xs={1} md={2}>
-                
-                <Col>
-                    <h2>
-                        Contact Info
-                    </h2>
-                    <ul>
-                        <li>
-                            <strong>e-mail:</strong> paolabramlett@gmail.com
-                        </li>
-                        <li>
-                            <strong>Phone number:</strong> +52 (951) 408 2852
-                        </li>
-                        <li>
-                        <strong><a href="https://www.instagram.com/paolabramlett/" target="_blank" rel="noreferrer">Instagram</a></strong>
-                        </li>
-                        <li>
-                            <strong><a href="https://www.linkedin.com/in/paolabramlett/" target="_blank" rel="noreferrer">LinkedIn</a></strong>
-                        </li>
-                    </ul>
-                </Col>
-                <Col>
-                
-                </Col>
-                
-                <Col>
-                <Form name="contact v1" method="post" data-netlify="true" onSubmit="submit" className="p-3">
-                    <input type="hidden" name="form-name" value="contact v1" />
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label className="text-white">E-mail address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Label className="text-white">Message</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" className="mt-3">
-                        Send
-                    </Button>
-                </Form>
-                </Col>
-                
-            </Row>
-        </Container>
+            <form 
+            name="conact-form" 
+            method="POST" 
+            data-netlify="true"
+            onSubmit="submit"
+            >
+                <input type="hidden" name="form-name" value="contact-form" />
+                <div>
+                    <label>Name <br />
+                        <input type="text" name="name" />
+                    </label>
+                    <br />
+                </div>
+                <div>
+                    <label htmlFor="email">e-mail: <br />
+                    <input id="email" type="email" name="email" />
+                    </label>
+                    <br />
+                </div>
+
+                <div>
+                    <label> Message: <br /> 
+                        <textarea name="message"></textarea>
+                    </label>
+                    <br />
+                </div>
+
+                <button type="submit">Send</button>
+
+            </form>
         </div>
     )
 }
