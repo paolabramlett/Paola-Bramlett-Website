188c0fef7e8301f6d4520a15128b47a844affb80
Paola Vasquez Bramlett <76532437+paolabramlett@users.noreply.github.com>
2021-02-08 13:36:30 -0600

    c

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -9,37 +9,7 @@ import "./ContactForm.css";
 export default function ContactForm (){
     return (
         <div>
-            <form 
-            name="conact" 
-            method="post" 
-            data-netlify="true"
-            onSubmit="submit"
-            netlify
-            >
-                <input type="hidden" name="form-name" value="contact" />
-                <div>
-                    <label>Name: <br />
-                        <input type="text" name="first-name" />
-                    </label>
-                    <br />
-                </div>
-                <div>
-                    <label htmlFor="email">e-mail: <br />
-                    <input id="email" type="email" name="email" />
-                    </label>
-                    <br />
-                </div>
-
-                <div>
-                    <label> Message: <br />
-                        <textarea name="comments"></textarea>
-                    </label>
-                    <br />
-                </div>
-
-                <button type="submit">Send</button>
-
-            </form>
+        
             <Container>
             <Row xs={1} md={2}>
                 
@@ -68,6 +38,7 @@ export default function ContactForm (){
                 
                 <Col>
                 <Form name="contact v1" method="post" data-netlify="true" onSubmit="submit" className="p-3">
+                    <input type="hidden" name="form-name" value="contact v1" />
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label className="text-white">Email address</Form.Label>
                         <Form.Control type="email" placeholder="name@example.com" />
